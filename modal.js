3d13dfb434eeec7f40475761864f15a5c896160d
matiasmvazquez <vazquezmatiasm@gmail.com>
2024-08-27 20:55:18 -0300

    Adaptado a mobil responsive y agregado Appsistencia

---
+++ modal.js
@@ -0,0 +1,11 @@
+// Open the Multi Modal
+    
+function openMultiModal(modal) {
+    document.getElementById(modal).style.display = "block";
+}
+
+// Close the Multi Modal
+
+function closeMultiModal(modal) {
+    document.getElementById(modal).style.display = "none";
+}
